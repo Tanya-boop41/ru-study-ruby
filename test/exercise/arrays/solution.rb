56525adf5ec93e1f036aca420d8a06a2206aca64
Tanya-boop41 <efita41@gmail.com>
2025-07-25 16:48:19 +0300

    Add solution task: replace numbers to array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,20 @@
 module Exercise
   module Arrays
     class << self
+
+      def max_positive_numb(array)
+        max = 0
+        array.each do |i|
+          max = i if i > max
+        end
+        max
+      end
+
       def replace(array)
-        array
+        n = max_positive_numb(array) 
+        array.map! do |i|
+          i > 0 && i < n ? n : i
+        end
       end
 
       def search(_array, _query)
