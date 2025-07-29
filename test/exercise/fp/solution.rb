87d73f61fd2cbc738cbcd6f2f3eb1ecdc80fcc0d
Tanya-boop41 <efita41@gmail.com>
2025-07-29 17:48:50 +0300

    Add solution task: rating calculation

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,13 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        ratings = _array.map do |film|
+          countries = film['country']&.split(',')&.map(&:strip)
+          rating = film['rating_kinopoisk'].to_f
+          countries && countries.size >=2 && rating > 0 ? rating : nil
+        end.compact
+        return 0 if ratings.empty?
+        ratings.reduce(:+) / ratings.size
       end
 
       def chars_count(_films, _threshold)
