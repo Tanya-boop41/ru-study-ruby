f967792f0cfff56de03533746de44806a3b3b781
Tanya-boop41 <efita41@gmail.com>
2025-08-10 14:50:19 +0300

    Add solution for task arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,32 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        array
+      def max_positive_numb(array)
+        max = 0
+        array.each do |i|
+          max = i if i > max
+        end
+        max
       end
 
-      def search(_array, _query)
-        0
+      def replace(array)
+        n = max_positive_numb(array) 
+        array.map! do |i|
+          i > 0 && i < n ? n : i
+        end
+      end
+
+      def search(_array, _query, left = 0, right = _array.length - 1)
+        return -1 if left > right
+
+        middle = (left + right) / 2
+        if _array[middle] == _query
+          middle
+        elsif _array[middle] < _query
+          search(_array, _query, middle + 1, right)
+        else
+          search(_array, _query, left, middle - 1)
+        end
       end
     end
   end
